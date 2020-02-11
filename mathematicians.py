f9bb6f9f904a137dc316c6652e12ded996fe0eb9
arsummers <aliyasummers1@gmail.com>
2020-02-10 16:38:58 -0800

    tries to scrape, does not come up with data for reasons mentioned in tutorial. May try again with different website

--- mathematicians.py
+++ mathematicians.py
@@ -53,13 +53,13 @@ def get_names():
     raise Exception('Error retrieving contents at {}'.format(url))
 
 def get_hits_on_name(name):
-     """
+    """
     Accepts a `name` of a mathematician and returns the number
     of hits that mathematician's Wikipedia page received in the 
     last 60 days, as an `int`
     """
     # url_root is a template string that is used to build a URL.
-    url_root = 'URL_REMOVED_SEE_NOTICE_AT_START_OF_ARTICLE'
+    url_root = 'http://www.fabpedigree.com/james/mathmen.htm'
     response = simple_get(url_root.format(name))
 
     if response is not None:
@@ -81,3 +81,39 @@ def get_hits_on_name(name):
     return None
 
 # MAIN CODE RUNNING HERE. EVERYTHING ABOVE IS SETUP
+if __name__ == "__main__":
+    print('Getting the list of name...')
+    names = get_names()
+    print('... done.\n')
+
+    results = []
+
+    print('getting stats for each name')
+    for name in names:
+        try:
+            hits = get_hits_on_name(name)
+            if hits is None:
+                hits = -1
+            results.append((hits, name))
+        except:
+            results.append((-1, name))
+            log_error('error encountered while processing '
+                    '{} skipping'.format(name))
+
+    print ('... done.\n')
+
+    results.sort()
+    results.reverse()
+
+    if len(results) > 5:
+        top_marks = results [:5]
+    else:
+        top_marks = results
+
+    print('\nThe most popular mathematicians are:\n')
+    for (mark, mathematician) in top_marks:
+        print('{} with {} pageviews'.format(mathematician, mark))
+
+    no_results = len([res for res in results if res[0] == -1])
+    print ('\nBut we did not find results for '
+        '{} mathematicians on the list'.format(no_results))
